84406636a9cf22ade34036de263f1206cb20985d
kLe0h <73725512+kLe0h@users.noreply.github.com>
2022-06-13 22:19:35 -0600

    Agregando las entidades y editando main Layout

---
+++ CapaEntidad/Carrito.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+        //    IdCarrito int primary key identity,
+        //IdCliente int references cliente(IdCliente),
+        //IdProducto int references producto(IdProducto),
+        //Cantidad int
+    public class Carrito
+    {
+        public int IdCarrito { get; set; }
+        public Cliente oCliente { get; set; }
+        public Producto oProducto { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
