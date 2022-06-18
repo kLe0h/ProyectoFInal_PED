215af77b6727bc0739974882eb7a7c40e150ce2e
kLe0h <mirandarodriguezleofernando@gmail.com>
2022-06-18 17:37:31 -0600

    Continuando pre-release

--- CapaTiendaWeb/App_Start/BundleConfig.cs
+++ CapaTiendaWeb/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace CapaTiendaWeb
                       "~/Scripts/bootstrap.bundle.js",
                       "~/Scripts/fontawesome/all.min.js",
                       "~/Scripts/loadingoverlay.min.js",
-                      "~/Scripts/sweetalert.js"
+                      "~/Scripts/sweetalert.min.js"
                       ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(            
